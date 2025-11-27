c11c58a5a3a3485db5c9c54976345643a14e7006
Irfani <369indonesia@gmail.com>
2025-11-27 18:27:07 +0700

    fix: pwa manifest config

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone', 
+
+        start_url: './',
+        scopes: './',
+        orientation: 'portrait',
+
         icons: [
           {
             src: 'pwa-192x192.png', 
